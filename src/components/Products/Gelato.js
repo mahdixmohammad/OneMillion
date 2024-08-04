a09b9e366c713de463d006d359aac403ea683f90
mahdixmohammad <mahdi3mohammad@gmail.com>
2024-08-04 19:14:36 -0400

    Fix gelato product

--- src/components/Products/Gelato.js
+++ src/components/Products/Gelato.js
@@ -1,7 +1,21 @@
 import { Link } from "react-router-dom";
-import SingleProduct from "./SingleProduct";
+import Product from "./Product";
+import strawberry from "../../assets/icons/gelato/strawberry.png";
+import lemon from "../../assets/icons/gelato/lemon.png";
+import bubblegum from "../../assets/icons/gelato/bubblegum.png";
+import vanilla from "../../assets/icons/gelato/vanilla.png";
+import ferreroRocher from "../../assets/icons/gelato/ferrero-rocher.png";
+import pistachio from "../../assets/icons/gelato/pistachio.png";
+import blueberry from "../../assets/icons/gelato/blueberry.png";
+import lotus from "../../assets/icons/gelato/lotus.png";
 
 const Gelato = () => {
+	const gelatoTypes = {
+		"Three Scoops": "4.0",
+		"0.5 KG": "7.0",
+		"1.0 KG": "14.0",
+	};
+
 	return (
 		<div className="products">
 			<h2>
@@ -24,6 +38,14 @@ const Gelato = () => {
 				</Link>
 				Gelato
 			</h2>
+			<Product name="Strawberry" icon={strawberry} types={gelatoTypes} />
+			<Product name="Lemon" icon={lemon} types={gelatoTypes} />
+			<Product name="Bubblegum" icon={bubblegum} types={gelatoTypes} />
+			<Product name="Vanilla" icon={vanilla} types={gelatoTypes} />
+			<Product name="Ferrero Rocher" icon={ferreroRocher} types={gelatoTypes} />
+			<Product name="Pistachio" icon={pistachio} types={gelatoTypes} />
+			<Product name="Blueberry" icon={blueberry} types={gelatoTypes} />
+			<Product name="Lotus" icon={lotus} types={gelatoTypes} />
 		</div>
 	);
 };
